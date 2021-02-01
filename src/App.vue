62a9586c7153a2c515f24233b476a84be49d0076
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-02-01 00:33:53 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  Test Cookie: {{ cookies.get('test') }}
+</template>
+
+<script lang="ts" setup>
+import { useCookies } from '@vueuse/integrations' 
+
+const cookies = useCookies()
+cookies.set('test', 'This is a test cookie')
+</script>
